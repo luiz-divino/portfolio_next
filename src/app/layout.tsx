0a7fdd906ba2f7e91ed2ecf9a80cb5ed0fc71577
luiz-divino <nandodivinodev@gmail.com>
2026-09-04 20:57:23 -0300

    chore: add vercel insights config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -25,6 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} ${rubik.variable} ${nunito.variable}  h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
