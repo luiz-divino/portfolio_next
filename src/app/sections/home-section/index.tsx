a187f0de234b8059f7fe8702cb055015418b42b3
luiz-divino <nandodivinodev@gmail.com>
2026-09-05 00:16:02 -0300

    refactor: remove padding bottom property

--- src/app/sections/home-section/index.tsx
+++ src/app/sections/home-section/index.tsx
@@ -6,7 +6,7 @@ const HomeSection = () => {
   return (
     <section className="h-dvh flex flex-col w-full items-center justify-center relative overflow-hidden bg-linear-to-r from-emerald-500 to-emerald-900">
       <Hero />
-      <TbArrowBigDownLinesFilled size={40} color="white" className="mb-4 animate-bounce"/>
+      <TbArrowBigDownLinesFilled size={40} color="white" className=" animate-bounce"/>
       <Floating3DParticles color="#66bf40" />
     </section>
   );
