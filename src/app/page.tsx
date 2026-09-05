f6cc6741b3e4b03f1c022ee98bd20efecc38c11c
luiz-divino <nandodivinodev@gmail.com>
2026-09-05 00:17:59 -0300

    feat: implement about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "./sections/about-section";
 import HomeSection from "./sections/home-section";
 
  const Home = () => {
   return(
     <main className="min-h-dvh w-full">
      <HomeSection/>
+     <About/>
     </main>
   )
 }
