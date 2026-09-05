86f45a9b79b592ace836bfe1779ca5862244b971
luiz-divino <nandodivinodev@gmail.com>
2026-09-05 03:04:44 -0300

    feat: implement projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./sections/about-section";
 import HomeSection from "./sections/home-section";
+import Projects from "./sections/projects-section";
 
  const Home = () => {
   return(
     <main className="min-h-dvh w-full">
      <HomeSection/>
      <About/>
+     <Projects/>
     </main>
   )
 }
