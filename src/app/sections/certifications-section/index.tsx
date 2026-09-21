cf2936a444944410adcc6d1a2d59f8c90fc36d24
luiz-divino <nandodivinodev@gmail.com>
2026-09-20 22:23:38 -0300

    refactor: change paddind bottom section

--- src/app/sections/certifications-section/index.tsx
+++ src/app/sections/certifications-section/index.tsx
@@ -4,7 +4,7 @@ import Title from "@/components/utilities/section-title";
 
 export const Certifications = () => {
     return(
-        <section className="max-w-7xl mx-auto pb:5 lg:pb-10">
+        <section className="max-w-7xl pb-10 mx-auto lg:pb-10">
             <Title title="Certificações" />
             <main className="w-full">
                 <Grid />
