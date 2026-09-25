d860a341e32cea41121e94850f978a1c32371b19
luiz-divino <nandodivinodev@gmail.com>
2026-09-25 19:22:03 -0300

    refactor: remove certificate image and add certificate title

--- src/components/certifications-components/card-certificate/index.tsx
+++ src/components/certifications-components/card-certificate/index.tsx
@@ -25,32 +25,27 @@ export interface CertificateProps {
 
 export const CardCertificate = ({ certificate }: CertificateProps) => {
   return (
-    <div className="min-h-full min-w-full">
-      <div className="relative overflow-hidden">
-        <Image
-          unoptimized
-          src={certificate.img}
-          priority
-          quality={100}
-          alt={""}
-          width={500}
-          height={500}
-          className="object-cover brightness-75 transition duration-300  max-h-72 ease-in-out hover:brightness-100 hover:scale-105"
-        />
-      </div>
-      <CardContent className="bg-emerald-700 rounded-b-2xl h-24">
-        <CardHeader>
-          <CardTitle className="px-3 max-w-xl text-center">
-            <h1 className="w-full flex justify-center font-nunito text-white text-md">
-              Utilizei os conhecimentos desse curso no(s) seguinte(s) projetos:
-            </h1>
+    <div className="w-[360px] lg:w-96 max-w-96 ">
+      <CardContent>
+        <CardHeader className="bg-emerald-200 h-20 lg:h-17 text-center flex flex-col items-center justify-center">
+          <CardTitle className="px-3 font-nunito font-semibold">
+            {certificate.title}
           </CardTitle>
+            <p className="font-rubik">{certificate.instituicao}</p>
         </CardHeader>
-        <CardDescription>
-          <div className="flex flex-wrap  gap-3 w-full lg:whitespace-nowrap items-center justify-center text-white">
+        <CardDescription className="bg-emerald-700">
+          <h1 className="flex justify-center font-normal text-white text-lg">
+            Projetos relacionados:
+          </h1>
+          <div className="flex flex-wrap gap-3 w-full lg:whitespace-nowrap items-center justify-center text-white">
             {certificate.projetos.map((item) => (
               <span key={item.url} className="py-3">
-                <Link href={item.url} className="font-bold outline-1 p-1 rounded-2xl">{item.nome}</Link>
+                <Link
+                  href={item.url}
+                  className="font-bold outline-1 p-1 rounded-2xl"
+                >
+                  {item.nome}
+                </Link>
               </span>
             ))}
           </div>
